f63aa8a8680581173dd5f74166171411283ac181
Lakshman Vadeghar <vadeghar@gmail.com>
2022-06-21 23:47:29 +0530

    Added a controller

--- src/main/java/org/trade/option/controller/HomeController.java
+++ src/main/java/org/trade/option/controller/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
         this.spotPriceService = spotPriceService;
         this.niftyService = niftyService;
     }
-    @GetMapping(value = { "/"})
+    @GetMapping(value = { "/","/index"})
     public String home(Model model) {
 
         model.addAttribute("df", DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"));
@@ -42,13 +42,6 @@ public class HomeController {
     }
     @GetMapping(value = { "/home" })
     public String homeViewPost(Model model) {
-//        List<OptionData> optionNiftyDataList = optionDataService.findAll(OcSymbolEnum.NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).atStartOfDay(), Sort.by("id").descending());
-//        List<OptionData> optionBankNiftyDataList = optionDataService.findAll(OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).atStartOfDay(), Sort.by("id").descending());
-//
-//        List<OptionData> niftyCeList = filter(optionNiftyDataList, OcSymbolEnum.NIFTY.getOhlcSymbol(), OptionTypeEnum.CE.name());
-//        List<OptionData> niftyPeList = filter(optionNiftyDataList, OcSymbolEnum.NIFTY.getOhlcSymbol(), OptionTypeEnum.PE.name());
-//        List<OptionData> bankNiftyCeList = filter(optionBankNiftyDataList, OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), OptionTypeEnum.CE.name());
-//        List<OptionData> bankNiftyPeList = filter(optionBankNiftyDataList, OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), OptionTypeEnum.PE.name());
         SpotPrice niftySpot = spotPriceService.getLastInserted(OcSymbolEnum.NIFTY.getOhlcSymbol());
         String inputDay = LocalDate.now(ZoneId.of("Asia/Kolkata")).format(formatter);
         List<Nifty> niftyList = niftyService.findAll(inputDay, Sort.by("id").descending());
@@ -56,7 +49,6 @@ public class HomeController {
         List<Nifty> niftyPeList = filter(niftyList, OptionTypeEnum.PE.name());
 
         Double niftySpotPrice = niftySpot.getLastPrice();
-//        Double bankNiftySpotPrice = bankNiftyCeList.get(0).getSpotPrice();
         String expDate = niftyCeList.get(0).getExpiry();
         model.addAttribute("niftyCeList", niftyCeList);
         model.addAttribute("niftyPeList", niftyPeList);
@@ -65,182 +57,25 @@ public class HomeController {
         model.addAttribute("niftySpotPrice", niftySpotPrice);
         model.addAttribute("expDate", expDate);
         model.addAttribute("df", DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"));
-//        model.addAttribute("bankNiftySpotPrice", bankNiftySpotPrice);
-//        model.addAttribute("bankNiftyCeList", bankNiftyCeList);
-//        model.addAttribute("bankNiftyPeList", bankNiftyPeList);
         model.addAttribute("active", 1);
         return "core/home";
     }
 
-
-    @GetMapping(value = { "analysis" })
-    public String analysis(Model model) {
-        model.addAttribute("active", 2);
-        return "core/analysis";
-    }
-    @GetMapping(value = {"/refresh"})
-    public @ResponseBody Map<String, Object> refresh() {
-        Map<String, Object> response = new HashMap<>();
-//        List<OptionData> optionNiftyDataList = optionDataService.findAll(OcSymbolEnum.NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).atStartOfDay(), Sort.by("id").descending());
-//        List<OptionData> optionBankNiftyDataList = optionDataService.findAll(OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).atStartOfDay(), Sort.by("id").descending());
+//    @GetMapping(value = {"/refreshIndex"})
+//    public @ResponseBody Map<String, List<SpotPrice>> refreshIndexPage() {
+//        Map<String, List<SpotPrice>> response = new HashMap<>();
 //
-//        List<OptionData> niftyCeList = filter(optionNiftyDataList, OcSymbolEnum.NIFTY.getOhlcSymbol(), OptionTypeEnum.CE.name());
-//        List<OptionData> niftyPeList = filter(optionNiftyDataList, OcSymbolEnum.NIFTY.getOhlcSymbol(), OptionTypeEnum.PE.name());
-//        List<OptionData> bankNiftyCeList = filter(optionBankNiftyDataList, OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), OptionTypeEnum.CE.name());
-//        List<OptionData> bankNiftyPeList = filter(optionBankNiftyDataList, OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), OptionTypeEnum.PE.name());
-
-        SpotPrice niftySpot = spotPriceService.getLastInserted(OcSymbolEnum.NIFTY.getOhlcSymbol());
-        String inputDay = LocalDate.now(ZoneId.of("Asia/Kolkata")).format(formatter);
-        List<Nifty> niftyList = niftyService.findAll(inputDay, Sort.by("id").descending());
-        List<Nifty> niftyCeList = filter(niftyList, OptionTypeEnum.CE.name());
-        List<Nifty> niftyPeList = filter(niftyList, OptionTypeEnum.PE.name());
-        Double niftySpotPrice = niftySpot.getLastPrice();
-        response.put("niftyCeList", niftyCeList);
-        response.put("niftyPeList", niftyPeList);
-        response.put("niftySpotPrice", niftySpotPrice);
-//        response.put("bankNiftyCeList", bankNiftyCeList);
-//        response.put("bankNiftyPeList", bankNiftyPeList);
-        return response;
-    }
-
-    @GetMapping(value = {"/refreshAnalysis"})
-    public @ResponseBody Map<String, Object> refreshAnalysis() {
-        SpotPrice niftySpot = spotPriceService.getLastInserted(OcSymbolEnum.NIFTY.getOhlcSymbol());
-        Integer atmStrike = ExpiryUtils.getATM(niftySpot.getLastPrice());
-        String inputDay = LocalDate.now(ZoneId.of("Asia/Kolkata")).format(formatter);
-        List<String> insertedTimeList = niftyService.getInsertedTimeList(inputDay, Sort.by("id").ascending());
-        List<Nifty> todayData = niftyService.findByUdatedAtSource(inputDay, Sort.by("id").ascending());
-        // Segment: 1, All In the money Call options
-        List<Map<Integer, List<Nifty>>> segment1 = new ArrayList<>();
-        Map<String, Object> response = new HashMap<>();
-        response.put("compartment1", prepareCompartment1(atmStrike, insertedTimeList, todayData, inputDay));
-        response.put("compartment2", prepareCompartment2(atmStrike, insertedTimeList, todayData, inputDay));
-        response.put("compartment3", prepareCompartment3(atmStrike, insertedTimeList, todayData, inputDay));
-        response.put("compartment4", prepareCompartment4(atmStrike, insertedTimeList, todayData, inputDay));
-
-        response.put("insertedTimeList", insertedTimeList);
-        response.put("niftyATM", atmStrike);
-        response.put("niftySpot", niftySpot.getUpdatedAtSource().replace(inputDay, "") +": "+ niftySpot.getLastPrice());
-        response.put("currentDate", inputDay);
-//        System.out.println("response: "+response);
-
-        return response;
-    }
-
-    private Map<Integer, Map> prepareCompartment1(Integer atmStrike, List<String> insertedTimeList, List<Nifty> todayData, String inputDay) {
-        Integer depth = 50;
-        Integer maxStrikePrice = atmStrike + (noOfStrikesPricesInEachCompartment * depth);
-        Integer minStrikePrice = atmStrike - (noOfStrikesPricesInEachCompartment * depth);
-
-        Map<Integer, List<Nifty>> strikeWiseData = todayData.stream()
-                .filter(nf -> nf.getStrikePrice() <= atmStrike && nf.getStrikePrice() > (minStrikePrice-50) && nf.getOptionType().equals("CE"))
-                .collect(Collectors.groupingBy(Nifty::getStrikePrice));
-        Map<Integer, Map> compartment1 = new HashMap<>();
-        for(Integer strikePrice: strikeWiseData.keySet()) {
-            Map<String, Long> insertedTimeItOi = new HashMap<>();
-            for(String insertedTime : insertedTimeList) {
-                insertedTimeItOi.put(insertedTime, getDataAtInsertedTime(strikeWiseData.get(strikePrice), insertedTime, inputDay));
-            }
-            Map<String, Long> result = insertedTimeItOi.entrySet().stream()
-                    .sorted(Map.Entry.comparingByKey())
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-            compartment1.put(strikePrice, result);
-        }
-        return compartment1;
-    }
-
-    private Map<Integer, Map> prepareCompartment2(Integer atmStrike, List<String> insertedTimeList, List<Nifty> todayData, String inputDay) {
-        Integer depth = 50;
-        Integer maxStrikePrice = atmStrike + (noOfStrikesPricesInEachCompartment * depth);
-        Integer minStrikePrice = atmStrike - (noOfStrikesPricesInEachCompartment * depth);
-        Map<Integer, List<Nifty>> strikeWiseData = todayData.stream()
-                .filter(nf -> nf.getStrikePrice() > atmStrike && nf.getStrikePrice() <= maxStrikePrice && nf.getOptionType().equals("CE"))
-                .collect(Collectors.groupingBy(Nifty::getStrikePrice));
-        Map<Integer, Map> compartment2 = new HashMap<>();
-        for(Integer strikePrice: strikeWiseData.keySet()) {
-            Map<String, Long> insertedTimeItOi = new HashMap<>();
-            for(String insertedTime : insertedTimeList) {
-                insertedTimeItOi.put(insertedTime, getDataAtInsertedTime(strikeWiseData.get(strikePrice), insertedTime, inputDay));
-            }
-            Map<String, Long> result = insertedTimeItOi.entrySet().stream()
-                    .sorted(Map.Entry.comparingByKey())
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-            compartment2.put(strikePrice, result);
-        }
-        return compartment2;
-    }
-
-    private Map<Integer, Map> prepareCompartment4(Integer atmStrike, List<String> insertedTimeList, List<Nifty> todayData, String inputDay) {
-        Integer depth = 50;
-        Integer maxStrikePrice = atmStrike + (noOfStrikesPricesInEachCompartment * depth);
-        Integer minStrikePrice = atmStrike - (noOfStrikesPricesInEachCompartment * depth);
-        Map<Integer, List<Nifty>> strikeWiseData = todayData.stream()
-                .filter(nf -> nf.getStrikePrice() > atmStrike && nf.getStrikePrice() <= maxStrikePrice && nf.getOptionType().equals("PE"))
-                .collect(Collectors.groupingBy(Nifty::getStrikePrice));
-        Map<Integer, Map> compartment3 = new HashMap<>();
-        for(Integer strikePrice: strikeWiseData.keySet()) {
-            Map<String, Long> insertedTimeItOi = new HashMap<>();
-            for(String insertedTime : insertedTimeList) {
-                insertedTimeItOi.put(insertedTime, getDataAtInsertedTime(strikeWiseData.get(strikePrice), insertedTime, inputDay));
-            }
-            Map<String, Long> result = insertedTimeItOi.entrySet().stream()
-                    .sorted(Map.Entry.comparingByKey())
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-            compartment3.put(strikePrice, result);
-        }
-        return compartment3;
-    }
-    private Map<Integer, Map> prepareCompartment3(Integer atmStrike, List<String> insertedTimeList, List<Nifty> todayData, String inputDay) {
-        Integer depth = 50;
-        Integer maxStrikePrice = atmStrike + (noOfStrikesPricesInEachCompartment * depth);
-        Integer minStrikePrice = atmStrike - (noOfStrikesPricesInEachCompartment * depth);
-        Map<Integer, List<Nifty>> strikeWiseData = todayData.stream()
-                .filter(nf -> nf.getStrikePrice() <= atmStrike && nf.getStrikePrice() > (minStrikePrice-50) && nf.getOptionType().equals("PE"))
-                .collect(Collectors.groupingBy(Nifty::getStrikePrice));
-        Map<Integer, Map> compartment4 = new HashMap<>();
-        for(Integer strikePrice: strikeWiseData.keySet()) {
-            Map<String, Long> insertedTimeItOi = new HashMap<>();
-            for(String insertedTime : insertedTimeList) {
-                insertedTimeItOi.put(insertedTime, getDataAtInsertedTime(strikeWiseData.get(strikePrice), insertedTime, inputDay));
-            }
-            Map<String, Long> result = insertedTimeItOi.entrySet().stream()
-                    .sorted(Map.Entry.comparingByKey())
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-            compartment4.put(strikePrice, result);
-        }
-        return compartment4;
-    }
-
-
-
-    private Long getDataAtInsertedTime(List<Nifty> niftyList, String insertedTime, String inputDay) {
-        Nifty nifty = niftyList.stream().filter(n -> n.getUpdatedAtSource().replace(inputDay,"").equals(insertedTime)).findFirst().orElse(null);
-        return nifty != null ? nifty.getChangeInOi() : 0;
-    }
-
-    @GetMapping(value = {"/refreshIndex"})
-    public @ResponseBody Map<String, List<SpotPrice>> refreshIndexPage() {
-        Map<String, List<SpotPrice>> response = new HashMap<>();
-
-        response.put("niftyToday", spotPriceService.getSpotPriceBySymbol(OcSymbolEnum.NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).format(formatter), Sort.by("id").ascending()));
-        response.put("bankNiftyToday", spotPriceService.getSpotPriceBySymbol(OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).format(formatter), Sort.by("id").ascending()));
-        return response;
-    }
-
-    private List<OptionData> filter(List<OptionData> optionDataList, String symbol, String optionType) {
-        return optionDataList.stream()
-                .filter(n -> n.getSymbol().startsWith(symbol) && n.getOptionType().equals(optionType))
-                .sorted(Comparator.comparing(OptionData::getId).reversed())
-                .collect(Collectors.toList());
-    }
+//        response.put("niftyToday", spotPriceService.getSpotPriceBySymbol(OcSymbolEnum.NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).format(formatter), Sort.by("id").ascending()));
+//        response.put("bankNiftyToday", spotPriceService.getSpotPriceBySymbol(OcSymbolEnum.BANK_NIFTY.getOhlcSymbol(), LocalDate.now(ZoneId.of("Asia/Kolkata")).format(formatter), Sort.by("id").ascending()));
+//        return response;
+//    }
+//
+//    private List<OptionData> filter(List<OptionData> optionDataList, String symbol, String optionType) {
+//        return optionDataList.stream()
+//                .filter(n -> n.getSymbol().startsWith(symbol) && n.getOptionType().equals(optionType))
+//                .sorted(Comparator.comparing(OptionData::getId).reversed())
+//                .collect(Collectors.toList());
+//    }
 
     private List<Nifty> filter(List<Nifty> optionDataList, String optionType) {
         return optionDataList.stream()
@@ -248,4 +83,6 @@ public class HomeController {
                 .sorted(Comparator.comparing(Nifty::getId).reversed())
                 .collect(Collectors.toList());
     }
+
+
 }
