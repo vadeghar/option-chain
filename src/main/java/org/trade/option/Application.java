2fa7e52c223f8a3585931ff3658d76765e417809
Lakshman Vadeghar <vadeghar@gmail.com>
2022-06-28 01:18:03 +0530

    Update Application.java

--- src/main/java/org/trade/option/Application.java
+++ src/main/java/org/trade/option/Application.java
@@ -3,8 +3,15 @@ package org.trade.option;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
